34014a94ba9bbf096194d1e293ec9e94da7dd3c6
Ram Krishna Paudel <ramkrishnaimmortal@gmail.com>
2025-05-20 12:14:58 +0545

    fix: vite.config.js base to /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/lovable-tagger/',
   server: {
     host: "::",
     port: 3000,
